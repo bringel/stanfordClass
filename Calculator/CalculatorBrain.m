50bc1ffb4a66d4698bcfc8967c76e9fc0ba4ace0
Brad Ringel <brad.ringel@gmail.com>
2012-03-13 14:45:43 -0400

    All the view controller methods and most of the brain methods implemented

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -19,27 +19,58 @@
 @synthesize programStack = _programStack;
 
 - (NSMutableArray *)programStack{
-    
+    if(_programStack == nil)
+        _programStack = [[NSMutableArray alloc] init];
+    return _programStack;
 }
 
 - (id)program{
-     
+    return [self.programStack copy];
 }
 
 - (void)pushOperand:(double)operand{
-    
+    [self.programStack addObject:[NSNumber numberWithDouble:operand]];
 }
 
 - (double)popOperandOffProgramStack:(NSMutableArray *)stack{
-    
+    return [[stack lastObject] doubleValue];
 }
 
 - (double)performOperation:(NSString *)operation{
+    double result;
     
+    if([operation isEqualToString:@"+"]){
+        result = [self popOperandOffProgramStack:[self programStack]] + [self popOperandOffProgramStack:[self programStack]];
+    }
+    else if([operation isEqualToString:@"-"]){
+        result = [self popOperandOffProgramStack:[self programStack]] - [self popOperandOffProgramStack:[self programStack]];
+    }
+    else if([operation isEqualToString:@"*"]){
+        result = [self popOperandOffProgramStack:[self programStack]] * [self popOperandOffProgramStack:[self programStack]];
+    }
+    else if([operation isEqualToString:@"/"]){
+        result = [self popOperandOffProgramStack:[self programStack]] / [self popOperandOffProgramStack:[self programStack]];
+    }
+    else if([operation isEqualToString:@"sin"]){
+        result = sin([self popOperandOffProgramStack:[self programStack]]);
+    }
+    else if([operation isEqualToString:@"cos"]){
+        result = cos([self popOperandOffProgramStack:[self programStack]]);
+    }
+    else if([operation isEqualToString:@"sqrt"]){
+        result = sqrt([self popOperandOffProgramStack:[self programStack]]);
+    }
+    else if([operation isEqualToString:@"π"]){
+        //result = [self popOperandOffProgramStack:[self programStack]] - [self popOperandOffProgramStack:[self programStack]];
+        result = M_PI;
+    }
+    
+    [self pushOperand:result];
+    return result;
 }
 
 - (void)clearProgramStack{
-    
+    [self setProgramStack:nil];
 }
 
 + (NSString *)descriptionOfProgram:(id)program{
@@ -47,6 +78,9 @@
 }
 
 + (double)runProgram:(id)program{
+    NSMutableArray * stack;
+    if([program isKindOfClass:[NSArray class]])
+        stack = [program copy];
     
 }
 
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -13,6 +13,7 @@
 
 @property (nonatomic) BOOL userIsEnteringNumber;
 @property (nonatomic) BOOL userHasPressedDecimal;
+@property (nonatomic) BOOL userHasChangedSign;
 @property (strong, nonatomic) CalculatorBrain * brain;
 
 @end
@@ -23,33 +24,78 @@
 @synthesize historyDisplay = _historyDisplay;
 @synthesize variablesDisplay = _variablesDisplay;
 @synthesize userIsEnteringNumber = _userIsEnteringNumber;
+@synthesize userHasChangedSign = _userHasChangedSign;
 @synthesize userHasPressedDecimal = _userHasPressedDecimal;
 @synthesize brain = _brain;
 
 - (CalculatorBrain *)brain{
-    
+    if(_brain == nil)
+        _brain = [[CalculatorBrain alloc] init];
+    return _brain;
 }
 
 - (IBAction)digitPressed:(UIButton *)sender {
-}
-
-- (IBAction)operatorPressed:(UIButton *)sender {
-    
+    if([self userIsEnteringNumber]){
+        [self.display setText:[self.display.text stringByAppendingString:[sender currentTitle]]];
+    }
+    else {
+        [self.display setText:[sender currentTitle]];
+        [self setUserIsEnteringNumber:YES];
+    }
 }
 
 - (IBAction)enterPressed {
+    [self.brain pushOperand:[[self.display text] doubleValue]];
+    [self.historyDisplay setText:[self.historyDisplay.text stringByAppendingFormat:@"%@ ",[self.display text]]];
+    [self setUserIsEnteringNumber:NO];
+    [self setUserHasPressedDecimal:NO];
+    [self setUserHasChangedSign:NO];
+}
+
+- (IBAction)operatorPressed:(UIButton *)sender {
+    [self enterPressed];
+    [self.historyDisplay setText:[self.historyDisplay.text stringByAppendingFormat:@"%@ ",[sender currentTitle]]];
+    [self.display setText:[NSString stringWithFormat:@"%g",[self.brain performOperation:[sender currentTitle]]]];
 }
 
 - (IBAction)decimalPressed {
+    if(![self userHasPressedDecimal]){
+        [self.display setText:[self.display.text stringByAppendingString:@"."]];
+        [self setUserHasPressedDecimal:YES];
+        [self setUserIsEnteringNumber:YES];
+    }
 }
 
 - (IBAction)signChangePressed {
+    if([self userHasChangedSign]){
+        [self.display setText:[self.display.text substringFromIndex:1]];
+        [self setUserHasChangedSign:NO];
+    }
+    else {
+        [self.display setText:[@"-" stringByAppendingString:[self.display text]]];
+        [self setUserHasChangedSign:YES];
+    }
 }
 
 - (IBAction)clearPressed {
+    [self.display setText:@"0"];
+    [self.historyDisplay setText:@"  "];
+    [self.brain clearProgramStack];
+    [self setUserHasChangedSign:NO];
+    [self setUserIsEnteringNumber:NO];
+    [self setUserHasPressedDecimal:NO];
 }
 
 - (IBAction)undoPressed {
+    if([self.display.text length] > 1){
+        [self.display setText:[self.display.text substringToIndex:[self.display.text length] - 1]];
+    }
+    else {
+        [self.display setText:@"0"];
+        [self setUserIsEnteringNumber:NO];
+        [self setUserHasPressedDecimal:NO];
+        [self setUserHasChangedSign:NO];
+    }
 }
 
 - (IBAction)variablePressed:(UIButton *)sender {
