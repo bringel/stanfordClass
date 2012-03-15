d964986dd6a95068cb0e30c43d07e59a0555dc4c
Brad Ringel <brad.ringel@gmail.com>
2012-03-15 19:46:05 -0400

    adding happiness project

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by Brad Ringel on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
