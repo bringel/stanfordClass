3e3959a1ad4962f170ac137043bac9500eb2ad4d
Brad Ringel <brad.ringel@gmail.com>
2012-03-16 10:14:14 -0400

    adding the faceview class

---
+++ Happiness/FaceView.h
@@ -0,0 +1,13 @@
+//
+//  FaceView.h
+//  Happiness
+//
+//  Created by Brad Ringel on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceView : UIView
+
+@end
