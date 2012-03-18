a2bc596851106b537ea93353086d029b6db38b59
Brad Ringel <brad.ringel@gmail.com>
2012-03-18 13:58:05 -0400

    adding the Psychologist project

---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by Brad Ringel on 3/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
