4cff36ce3a03e401efd8d2b2b019b0e1251fac90
Brad Ringel <brad.ringel@gmail.com>
2012-03-23 23:55:38 -0400

    iPhone version can rotate and iPad storyboard is added

---
+++ Psychologist/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Psychologist
+//
+//  Created by Brad Ringel on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ Psychologist/RotatableViewController.m
@@ -0,0 +1,23 @@
+//
+//  RotatableViewController.m
+//  Psychologist
+//
+//  Created by Brad Ringel on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@interface RotatableViewController ()
+
+@end
+
+@implementation RotatableViewController
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
