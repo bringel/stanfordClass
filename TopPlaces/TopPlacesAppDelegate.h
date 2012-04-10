b0ace5bc3862fef3656a4063337c3259d59de408
Brad Ringel <brad.ringel@gmail.com>
2012-04-09 22:35:52 -0400

    Starting the Top Places Flickr Project

---
+++ TopPlaces/TopPlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TopPlacesAppDelegate.h
+//  TopPlaces
+//
+//  Created by Brad Ringel on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/TopPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesViewController.h
+//  TopPlaces
+//
+//  Created by Brad Ringel on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesViewController : UIViewController
+
+@end
